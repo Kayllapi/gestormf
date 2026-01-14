8be98285af6fd4f11487f0262719f0f1594113ef
abrahamalanya <alanyaoficial@gmail.com>
2026-01-13 19:31:27 -0500

    compraventa update

--- app/Http/Controllers/Layouts/Backoffice/Sistema/CompraventaController.php
+++ app/Http/Controllers/Layouts/Backoffice/Sistema/CompraventaController.php
@@ -14,12 +14,25 @@ class CompraventaController extends Controller
 
         $tienda = DB::table('tienda')->whereId($idtienda)->first();
         $agencias = DB::table('tienda')->get();
-        $tipo_garantia = DB::table('tipo_garantia')->get();
-        $estado_garantia = DB::table('estado_garantia')->get();
-        $bancos = DB::table('banco')->get();
 
         if(request('view') == 'tabla'){
             return view(sistema_view().'/compraventa/tabla', compact(
+                'tienda',
+                'agencias',
+            ));
+        }
+    }
+
+    public function create(Request $request,$idtienda)
+    {
+        if (request('view') == 'create_compra') {
+            $tienda = DB::table('tienda')->whereId($idtienda)->first();
+            $agencias = DB::table('tienda')->get();
+            $tipo_garantia = DB::table('tipo_garantia')->get();
+            $estado_garantia = DB::table('estado_garantia')->get();
+            $bancos = DB::table('banco')->get();
+
+            return view(sistema_view().'/compraventa/create_compra', compact(
                 'tienda',
                 'agencias',
                 'tipo_garantia',
@@ -29,50 +42,269 @@ class CompraventaController extends Controller
         }
     }
 
-    public function create(Request $request,$idtienda)
-    {
-    }
-
     public function store(Request $request, $idtienda)
     {
-        if(request('view') == 'registrar'){
+        if (request('view') == 'registrar') {
             $rules = [
-                'margen_previsto' => 'required',
-                'valor_descuento' => 'required',
+                'idtienda' => 'required',
+                'idtipogarantia' => 'required',
+                'descripcion' => 'required',
+                'serie_motor_partida' => 'required',
+                'modelo_tipo' => 'required',
+                'idestado_garantia' => 'required',
+                'color' => 'required',
+                'valorcompra' => 'required',
+                'valorcomercial' => 'required',
+                'vendedor_nombreapellidos' => 'required',
+                'vendedor_dni' => 'required',
+                'idorigen' => 'required',
+                'numeroficha' => 'required',
             ];
             $messages = [
-                'margen_previsto.required' => 'El "Margen Previsto" es Obligatorio.',
-                'valor_descuento.required' => 'El "Valor de Descuento" es Obligatorio.',
+                'idtienda.required' => 'El "Agencia" es Obligatorio.',
+                'idtipogarantia.required' => 'El "Tipo de Bien" es Obligatorio.',
+                'descripcion.required' => 'La "Descripción" es Obligatorio.',
+                'serie_motor_partida.required' => 'El "Serie/Motor/N° Partida" es Obligatorio.',
+                'modelo_tipo.required' => 'El "Modelo/Tipo" es Obligatorio.',
+                'idestado_garantia.required' => 'El "Estado" es Obligatorio.',
+                'color.required' => 'El "Color" es Obligatorio.',
+                'valorcompra.required' => 'El "Valor Compra (soles)" es Obligatorio.',
+                'valorcomercial.required' => 'El "Valor Comercial" es Obligatorio.',
+                'vendedor_nombreapellidos.required' => 'El "Apellidos y Nombres (Vendedor)" es Obligatorio.',
+                'vendedor_dni.required' => 'El "DNI (Vendedor)" es Obligatorio.',
+                'idorigen.required' => 'El "Origen" es Obligatorio.',
+                'numeroficha.required' => 'El "N° de Ficha o Comprobante" es Obligatorio.',
             ];
-            $this->validate($request,$rules,$messages);
 
-            if(request('margen_previsto') <= request('valor_descuento')){
-               return response()->json([
-                    'resultado' => 'ERROR',
-                    'mensaje'   => 'El margen previsto debe ser mayor al valor de descuento.'
-                ]); 
+            if (request('compra_idformapago') == 2) {
+                $rules['compra_numerooperacion'] = 'required';
+                $rules['compra_idbanco'] = 'required';
+                $messages['compra_numerooperacion.required'] = 'El "N° de Operación" es Obligatorio.';
+                $messages['compra_idbanco.required'] = 'El "Banco" es Obligatorio.';
             }
 
-            configuracion_update($idtienda,'valor_descuento', request('valor_descuento'));
-            configuracion_update($idtienda,'margen_previsto', request('margen_previsto'));
+            $this->validate($request,$rules,$messages);
+
+            $codigo = DB::table('cvcompra')->max('codigo') + 1;
+
+            // Banco
+            $banco = DB::table('banco')->where('id',request('compra_idbanco'))->first();
+            $compra_banco = $banco->nombre ?? '';
+            $compra_cuenta = $banco->cuenta ?? '';
+
+            DB::table('cvcompra')->insert([
+                'idtipogarantia' => request('idtipogarantia'),
+                'fecharegistro' => now(),
+                'codigo' => $codigo,
+                'descripcion' => request('descripcion'),
+                'serie_motor_partida' => request('serie_motor_partida'),
+                'chasis' => request('chasis'),
+                'modelo_tipo' => request('modelo_tipo'),
+                'otros' => request('otros'),
+                'idestado_garantia' => request('idestado_garantia'),
+                'color' => request('color'),
+                'fabricacion' => request('fabricacion'),
+                'compra' => request('compra'),
+                'placa' => request('placa'),
+                'valorcompra' => request('valorcompra'),
+                'valorcomercial' => request('valorcomercial'),
+                'accesorio_doc' => '',
+                'detalle_garantia' => '',
+                'vendedor_nombreapellidos' => request('vendedor_nombreapellidos'),
+                'vendedor_dni' => request('vendedor_dni'),
+                'idorigen' => request('idorigen'),
+                'numeroficha' => request('numeroficha'),
+                'compra_numerooperacion' => request('compra_numerooperacion') ?? '',
+                'compra_banco' => $compra_banco,
+                'compra_cuenta' => $compra_cuenta,
+                'compra_idbanco' => request('compra_idbanco'),
+                'compra_idformapago' => request('compra_idformapago'),
+                'validar_estado' => 0,
+                'validar_responsable' => 0,
+                'validar_responsable_permiso' => 0,
+                'idcvarqueocaja_cierre' => 0,
+                'idresponsable' => auth()->user()->id,
+                'idresponsable_permiso' => 0,
+                'eliminado_idresponsable' => 0,
+                'eliminado_idresponsable_permiso' => 0,
+                'idestadocvcompra' => 1,
+                'idestadoeliminado' => 1,
+                'idtienda' => request('idtienda'),
+            ]);
 
             return response()->json([
                 'resultado' => 'CORRECTO',
-                'mensaje'   => 'Se ha actualizado correctamente.'
+                'mensaje'   => 'Se ha registrado correctamente.'
             ]); 
         }
     }
 
     public function show(Request $request, $idtienda, $id)
     {
+        if ($id=='show_table_compra') {
+            $where = [];
+
+            // request('check_compra')
+            if(request('id_agencia_compra') != ''){
+                $where[] = ['idtienda', '=', request('id_agencia_compra')];
+            }
+            if(request('fecha_inicio_compra') != ''){
+                $where[] = ['fecharegistro','>=',request('fecha_inicio_compra').' 00:00:00'];
+            }
+            if(request('fecha_fin_compra') != ''){
+                $where[] = ['fecharegistro','<=',request('fecha_fin_compra').' 23:59:59'];
+            }
+
+            $cvcompras = DB::table('cvcompra')
+                ->where($where)
+                ->orderBy('fecharegistro')
+                ->get();
+  
+            $total = 0;
+            $html = '';
+            foreach($cvcompras as $value){
+                $fecharegistro = date_format(date_create($value->fecharegistro),"d-m-Y H:i:s A");
+                $origen = $value->idorigen == '1' ? 'SERFIP' : 'OTROS';
+
+                $html .= "<tr data-valor-columna='{$value->id}' onclick='show_data_compra(this)'>
+                            <td></td>
+                            <td>CB{$value->codigo}</td>
+                            <td>{$value->descripcion}</td>
+                            <td>{$value->serie_motor_partida}</td>
+                            <td>{$value->modelo_tipo}</td>
+                            <td>{$fecharegistro}</td>
+                            <td>{$value->valorcompra}</td>
+                            <td>{$value->valorcomercial}</td>
+                            <td>{$value->chasis}</td>
+                            <td>{$origen}</td>
+                            <td>{$value->numeroficha}</td>
+                        </tr>";
+                $total = $total+$value->valorcompra;
+            }
+
+            if(count($cvcompras)==0){
+                $html.= '<tr><td colspan="11" style="text-align: center;font-weight: bold;">No hay ningún dato!!</td></tr>';
+            }
+
+            return array(
+                'html' => $html,
+                'total' => number_format($total, 2, '.', ''),
+            );
+        }
     }
 
     public function edit(Request $request, $idtienda, $id)
     {
+        if (request('view') == 'editar_compra') {
+            $tienda = DB::table('tienda')->whereId($idtienda)->first();
+            $agencias = DB::table('tienda')->get();
+            $tipo_garantia = DB::table('tipo_garantia')->get();
+            $estado_garantia = DB::table('estado_garantia')->get();
+            $bancos = DB::table('banco')->get();
+            $cvcompra = DB::table('cvcompra')->whereId($id)->first();
+
+            return view(sistema_view().'/compraventa/edit_compra', compact(
+                'tienda',
+                'agencias',
+                'tipo_garantia',
+                'estado_garantia',
+                'bancos',
+                'cvcompra',
+            ));
+        }
     }
 
     public function update(Request $request, $idtienda, $id)
     {
+        if (request('view') == 'editar_compra') {
+            $rules = [
+                'idtienda' => 'required',
+                'idtipogarantia' => 'required',
+                'descripcion' => 'required',
+                'serie_motor_partida' => 'required',
+                'modelo_tipo' => 'required',
+                'idestado_garantia' => 'required',
+                'color' => 'required',
+                'valorcompra' => 'required',
+                'valorcomercial' => 'required',
+                'vendedor_nombreapellidos' => 'required',
+                'vendedor_dni' => 'required',
+                'idorigen' => 'required',
+                'numeroficha' => 'required',
+            ];
+            $messages = [
+                'idtienda.required' => 'El "Agencia" es Obligatorio.',
+                'idtipogarantia.required' => 'El "Tipo de Bien" es Obligatorio.',
+                'descripcion.required' => 'La "Descripción" es Obligatorio.',
+                'serie_motor_partida.required' => 'El "Serie/Motor/N° Partida" es Obligatorio.',
+                'modelo_tipo.required' => 'El "Modelo/Tipo" es Obligatorio.',
+                'idestado_garantia.required' => 'El "Estado" es Obligatorio.',
+                'color.required' => 'El "Color" es Obligatorio.',
+                'valorcompra.required' => 'El "Valor Compra (soles)" es Obligatorio.',
+                'valorcomercial.required' => 'El "Valor Comercial" es Obligatorio.',
+                'vendedor_nombreapellidos.required' => 'El "Apellidos y Nombres (Vendedor)" es Obligatorio.',
+                'vendedor_dni.required' => 'El "DNI (Vendedor)" es Obligatorio.',
+                'idorigen.required' => 'El "Origen" es Obligatorio.',
+                'numeroficha.required' => 'El "N° de Ficha o Comprobante" es Obligatorio.',
+            ];
+
+            if (request('compra_idformapago') == 2) {
+                $rules['compra_numerooperacion'] = 'required';
+                $rules['compra_idbanco'] = 'required';
+                $messages['compra_numerooperacion.required'] = 'El "N° de Operación" es Obligatorio.';
+                $messages['compra_idbanco.required'] = 'El "Banco" es Obligatorio.';
+            }
+
+            $this->validate($request,$rules,$messages);
+
+            // Banco
+            $banco = DB::table('banco')->where('id',request('compra_idbanco'))->first();
+            $compra_banco = $banco->nombre ?? '';
+            $compra_cuenta = $banco->cuenta ?? '';
+
+            DB::table('cvcompra')->whereId($id)->update([
+                'idtipogarantia' => request('idtipogarantia'),
+                'descripcion' => request('descripcion'),
+                'serie_motor_partida' => request('serie_motor_partida'),
+                'chasis' => request('chasis'),
+                'modelo_tipo' => request('modelo_tipo'),
+                'otros' => request('otros'),
+                'idestado_garantia' => request('idestado_garantia'),
+                'color' => request('color'),
+                'fabricacion' => request('fabricacion'),
+                'compra' => request('compra'),
+                'placa' => request('placa'),
+                'valorcompra' => request('valorcompra'),
+                'valorcomercial' => request('valorcomercial'),
+                'accesorio_doc' => '',
+                'detalle_garantia' => '',
+                'vendedor_nombreapellidos' => request('vendedor_nombreapellidos'),
+                'vendedor_dni' => request('vendedor_dni'),
+                'idorigen' => request('idorigen'),
+                'numeroficha' => request('numeroficha'),
+                'compra_numerooperacion' => request('compra_numerooperacion') ?? '',
+                'compra_banco' => $compra_banco,
+                'compra_cuenta' => $compra_cuenta,
+                'compra_idbanco' => request('compra_idbanco'),
+                'compra_idformapago' => request('compra_idformapago'),
+                'validar_estado' => 0,
+                'validar_responsable' => 0,
+                'validar_responsable_permiso' => 0,
+                'idcvarqueocaja_cierre' => 0,
+                'idresponsable' => auth()->user()->id,
+                'idresponsable_permiso' => 0,
+                'eliminado_idresponsable' => 0,
+                'eliminado_idresponsable_permiso' => 0,
+                'idestadocvcompra' => 1,
+                'idestadoeliminado' => 1,
+                'idtienda' => request('idtienda'),
+            ]);
+
+            return response()->json([
+                'resultado' => 'CORRECTO',
+                'mensaje'   => 'Se ha actualizado correctamente.'
+            ]); 
+        }
     } 
 
     public function destroy(Request $request, $idtienda, $id)
---
+++ resources/views/layouts/backoffice/sistema/compraventa/edit_compra.blade.php
@@ -0,0 +1,174 @@
+<form action="javascript:;"
+    id="form-editar-compraventa"  
+    onsubmit="callback({
+        route: '{{ url('backoffice/'.$tienda->id.'/compraventa/'.$cvcompra->id) }}',
+        method: 'PUT',
+        data:{
+            view: 'editar_compra',
+        }
+    },
+    function(resultado){
+        $('#modal-close-edit-compraventa').click();
+        search_compra();
+    },this)">
+    <div class="modal-header">
+        <h1 class="modal-title">Editar Compra de Bienes</h1>
+        <button type="button" class="btn-close" id="modal-close-edit-compraventa" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <div class="modal-body p-1">
+        <div class="row">
+            <label class="col-sm-4 col-form-label" style="text-align: right;">Agencia *</label>
+            <div class="col-sm-8">
+                <select class="form-control" id="idtienda">
+                    <option></option>
+                    @foreach($agencias as $value)
+                        <option value="{{$value->id}}">{{$value->nombreagencia}}</option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+        <div class="row">
+            <label class="col-sm-4 col-form-label" style="text-align: right;">Tipo de Bien *</label>
+            <div class="col-sm-8">
+                <select class="form-control" id="idtipogarantia">
+                    <option></option>
+                    @foreach($tipo_garantia as $value)
+                        <option value="{{ $value->id }}" antiguedad="{{ $value->antiguedad}}" valor="{{ $value->valor}}" cobertura="{{ $value->cobertura}}">{{ $value->nombre }}</option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <label class="col-form-label" style="text-align: right;">Descripción *</label>
+                <input type="text" class="form-control" id="descripcion" value="{{$cvcompra->descripcion}}">
+            </div>
+            <div class="col-sm-6">
+                <label class="col-form-label" style="text-align: right;">Serie/Motor/N° Partida *</label>
+                <input type="text" class="form-control" id="serie_motor_partida" value="{{$cvcompra->serie_motor_partida}}">
+            </div>
+            <div class="col-sm-6">
+                <label class="col-form-label" style="text-align: right;">Chasis</label>
+                <input type="text" class="form-control" id="chasis" value="{{$cvcompra->chasis}}">
+            </div>
+            <div class="col-sm-6">
+                <label class="col-form-label" style="text-align: right;">Modelo/Tipo *</label>
+                <input type="text" class="form-control" id="modelo_tipo" value="{{$cvcompra->modelo_tipo}}">
+            </div>
+            <div class="col-sm-6">
+                <label class="col-form-label" style="text-align: right;">Otros/Contraseña</label>
+                <input type="text" class="form-control" id="otros" value="{{$cvcompra->otros}}">
+            </div>
+            <div class="col-sm-6">
+                <label class="col-form-label" style="text-align: right;">Estado *</label>
+                <select class="form-control" id="idestado_garantia">
+                    <option></option>
+                    @foreach($estado_garantia as $value)
+                        <option value="{{$value->id}}">{{$value->nombre}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="col-sm-6">
+                <label class="col-form-label" style="text-align: right;">Color *</label>
+                <input type="text" class="form-control" id="color" value="{{$cvcompra->color}}">
+            </div>
+            <div class="col-sm-3">
+                <label class="col-form-label" style="text-align: right;">Año Fabricación</label>
+                <input type="text" class="form-control" id="fabricacion" value="{{$cvcompra->fabricacion}}">
+            </div>
+            <div class="col-sm-3">
+                <label class="col-form-label" style="text-align: right;">Año Compra</label>
+                <input type="text" class="form-control" id="compra" value="{{$cvcompra->compra}}">
+            </div>
+            <div class="col-sm-6">
+                <label class="col-form-label" style="text-align: right;">Placa (Vehículos)</label>
+                <input type="text" class="form-control" id="placa" value="{{$cvcompra->placa}}">
+            </div>
+            <div class="col-sm-6">
+                <label class="col-form-label" style="text-align: right;">Valor Compra (soles) *</label>
+                <input type="number" step="any" class="form-control" id="valorcompra" value="{{$cvcompra->valorcompra}}">
+            </div>
+            <div class="col-sm-6">
+                <label class="col-form-label" style="text-align: right;">Valor Comercial ({{ configuracion($tienda->id,'margen_previsto')['valor'] }}%) *</label>
+                <input type="number" step="any" class="form-control" id="valorcomercial" value="{{$cvcompra->valorcomercial}}">
+            </div>
+            <div class="col-sm-6">
+                <label class="col-form-label" style="text-align: right;">Apellidos y Nombres (Vendedor) *</label>
+                <input type="text" class="form-control" id="vendedor_nombreapellidos" value="{{$cvcompra->vendedor_nombreapellidos}}">
+            </div>
+            <div class="col-sm-6">
+                <label class="col-form-label" style="text-align: right;">DNI (Vendedor) *</label>
+                <input type="text" class="form-control" id="vendedor_dni" value="{{$cvcompra->vendedor_dni}}">
+            </div>
+            <div class="col-sm-3">
+                <label class="col-form-label" style="text-align: right;">Origen *</label>
+                <select class="form-control" id="idorigen">
+                    <option></option>
+                    <option value="1">SERFIP</option>
+                    <option value="2">OTROS</option>
+                </select>
+            </div>
+            <div class="col-sm-5">
+                <label class="col-form-label" style="text-align: right;">N° de Ficha o Comprobante *</label>
+                <input type="text" class="form-control" id="numeroficha" value="{{$cvcompra->numeroficha}}">
+            </div>
+            <div class="col-sm-4">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <input type="radio" name="compra_idformapago" id="compra_idformapago" value="1" @if($cvcompra->compra_idformapago == 1) checked @endif> Caja
+                        <input type="radio" name="compra_idformapago" id="compra_idformapago" value="2" @if($cvcompra->compra_idformapago == 2) checked @endif> Banco
+                    </div>
+                    <div class="col-sm-12">
+                        <label class="col-form-label" style="text-align: right;">N° Operación</label>
+                        <input type="text"
+                            class="form-control"
+                            id="compra_numerooperacion"
+                            value="{{$cvcompra->compra_numerooperacion}}"
+                            @if($cvcompra->compra_idformapago == 1) disabled @endif>
+                    </div>
+                    <div class="col-sm-12">
+                        <label class="col-form-label" style="text-align: right;">Bancos</label>
+                        <select class="form-control"
+                            id="compra_idbanco"
+                            @if($cvcompra->compra_idformapago == 1) disabled @endif>
+                            <option></option>
+                            @foreach($bancos as $value)
+                                <option value="{{$value->id}}">{{$value->nombre}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal-footer">
+        <button type="submit" class="btn btn-primary">
+            <i class="fa-solid fa-floppy-disk"></i> Guardar Cambios
+        </button>
+    </div>
+</form>
+
+<script>
+    sistema_select2({ input:'#idtienda', val:'{{$cvcompra->idtienda}}' });
+    sistema_select2({ input:'#idtipogarantia', val:'{{$cvcompra->idtipogarantia}}' });
+    sistema_select2({ input:'#idestado_garantia', val:'{{$cvcompra->idestado_garantia}}' });
+    sistema_select2({ input:'#idorigen', val:'{{$cvcompra->idorigen}}' });
+    sistema_select2({ input:'#compra_idbanco', val:'{{$cvcompra->compra_idbanco}}' });
+
+    $('input[name="compra_idformapago"]').on('change', function() {
+        if($(this).val() == 1){
+            $('#compra_numerooperacion').prop('disabled', true);
+            $('#compra_idbanco').prop('disabled', true);
+        }else{
+            $('#compra_numerooperacion').prop('disabled', false);
+            $('#compra_idbanco').prop('disabled', false);
+        }
+    });
+
+    $('#valorcompra').on('input', function() {
+        var valorCompra = parseFloat($(this).val()) || 0;
+        var margenPrevisto = parseFloat({{ configuracion($tienda->id,'margen_previsto')['valor'] }}) || 0;
+        var valorComercial = valorCompra * ((margenPrevisto / 100) + 1);
+        $('#valorcomercial').val(valorComercial.toFixed(2));
+    });
+</script>

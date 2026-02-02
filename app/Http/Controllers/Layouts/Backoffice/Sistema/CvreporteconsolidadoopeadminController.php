11a032ce1a3b2eea0b71268e2366539a75ca5f2d
abrahamalanya <alanyaoficial@gmail.com>
2026-02-02 10:56:50 -0500

    reporte

--- app/Http/Controllers/Layouts/Backoffice/Sistema/CvreporteconsolidadoopeadminController.php
+++ app/Http/Controllers/Layouts/Backoffice/Sistema/CvreporteconsolidadoopeadminController.php
@@ -48,7 +48,7 @@ class CvreporteconsolidadoopeadminController extends Controller
             $date = Carbon::createFromFormat('Y-m-d', $request->corte);
             $date->subDay(); // Subtracts 1 day
             //$co_anterior = consolidadooperaciones($tienda,$request->idagencia,$date->format('Y-m-d'));
-            $co_anterior = DB::table('arqueocaja')
+            $co_anterior = DB::table('cvarqueocaja')
                 ->where('idagencia',$request->idagencia)
                 ->where('corte',$date->format('Y-m-d'))
                 ->first();

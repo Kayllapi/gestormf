08fd549181d76fc8f63cab931cd52c47e51e6b7a
abrahamalanya <alanyaoficial@gmail.com>
2026-02-03 11:04:23 -0500

    alerta

--- resources/views/app/nuevosistema/alerta.blade.php
+++ resources/views/app/nuevosistema/alerta.blade.php
@@ -3,8 +3,12 @@
         <button type="button" class="btn-close" id="modal-close-usuario-autorizacion" data-bs-dismiss="modal" aria-label="Close"></button>
     </div>
     <div class="modal-body">
-        <div class="alert bg-danger" style="color: #c31717 !important; background-color: #fde1e2 !important;">
+        {{-- <div class="alert bg-danger" style="color: #c31717 !important; background-color: #fde1e2 !important;">
         <i class="fa fa-warning" style="font-size: 35px;color: #c31717;"></i> <br>
         {{$mensaje}}
+        </div> --}}
+        <div class="alert alert-danger">
+          <i class="fa-solid fa-triangle-exclamation"></i>
+          <b>{{$mensaje}}</b>
         </div>
     </div>

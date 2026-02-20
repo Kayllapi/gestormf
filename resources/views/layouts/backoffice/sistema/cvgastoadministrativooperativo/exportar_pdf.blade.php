1fdd1fb69e0238f0ab46d4334448b7695dccbff1
abrahamalanya <alanyaoficial@gmail.com>
2026-02-19 19:52:03 -0500

    pdf

--- resources/views/layouts/backoffice/sistema/cvgastoadministrativooperativo/exportar_pdf.blade.php
+++ resources/views/layouts/backoffice/sistema/cvgastoadministrativooperativo/exportar_pdf.blade.php
@@ -116,8 +116,8 @@
   </footer>
   <main>
     <div class="container">
-      <h4 align="center" style="font-size:16px; margin: 0px;">GASTOS ADMINISTRATIVOS Y  OPERATIVOS</h4>
-      <h4 align="center" style="font-size:16px; margin: 0px;">COMPRA Y VENTA DE BIENES</h4>
+      <h4 align="center" style="font-size:15px; margin: 0px;">GASTOS ADMINISTRATIVOS Y  OPERATIVOS</h4>
+      <h4 align="center" style="font-size:15px; margin: 0px;">COMPRA Y VENTA DE BIENES</h4>
            <b>DE: </b>{{ $fechainicio }}<br>
            <b>HASTA: </b>{{ $fechafin }}<br>
             

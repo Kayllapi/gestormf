393f72af12b355ba7c27428819cdfb8ada907ff0
abrahamalanya <alanyaoficial@gmail.com>
2026-02-20 20:16:21 -0500

    credito

--- resources/views/layouts/backoffice/sistema/carteradeclienteasesor/tabla.blade.php
+++ resources/views/layouts/backoffice/sistema/carteradeclienteasesor/tabla.blade.php
@@ -166,7 +166,7 @@
 </style>
 <script>
 
-  sistema_select2({ input:'#idagencia' });
+  sistema_select2({ input:'#idagencia', val:'{{$tienda->id}}' });
   sistema_select2({ input:'#idasesor' });
   sistema_select2({ input:'#idformacredito' });
 

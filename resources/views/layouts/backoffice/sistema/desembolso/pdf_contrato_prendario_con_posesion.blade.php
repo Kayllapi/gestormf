0dd38b62d8152d21e338ecfc1f75c0714435d159
abrahamalanya <alanyaoficial@gmail.com>
2026-04-16 17:38:17 -0500

    desembolso, contrato con posesion pdf

--- resources/views/layouts/backoffice/sistema/desembolso/pdf_contrato_prendario_con_posesion.blade.php
+++ resources/views/layouts/backoffice/sistema/desembolso/pdf_contrato_prendario_con_posesion.blade.php
@@ -8,7 +8,7 @@ CON GARANTÍA MOBILIARIA CON POSESIÓN</title>
     <style>
       *{
         font-family:helvetica;
-        font-size:12px;
+        font-size:11.5px;
       }
       @page {
           margin: 0cm 0cm;

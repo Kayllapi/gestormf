8e7088e1a8a8d337973041c3cab8ef33e4782003
abrahamalanya <alanyaoficial@gmail.com>
2026-04-17 16:32:28 -0500

    credito, formato evaluación

--- resources/views/layouts/backoffice/sistema/credito/formato_evaluacion.blade.php
+++ resources/views/layouts/backoffice/sistema/credito/formato_evaluacion.blade.php
@@ -550,7 +550,7 @@
         @endif
         <div class="col" style="flex: 0 0 0%;">
           <button type="button" 
-                  class="btn btn-dark" 
+                  class="btn btn-info" 
                   onclick="modal({ route:'{{url('backoffice/'.$tienda->id.'/credito/'.$credito->id.'/edit?view=solicitud_formato_evaluacion')}}', size: 'modal-fullscreen' })"
                   id="boton_imprimir"
                   >

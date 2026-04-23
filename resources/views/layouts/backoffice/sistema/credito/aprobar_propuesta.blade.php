2ba2133671f9c1ab316d31505395e4d68339ebd1
abrahamalanya <alanyaoficial@gmail.com>
2026-04-23 17:23:17 -0500

    aprobar propuesta

--- resources/views/layouts/backoffice/sistema/credito/aprobar_propuesta.blade.php
+++ resources/views/layouts/backoffice/sistema/credito/aprobar_propuesta.blade.php
@@ -141,61 +141,64 @@
               ?>
       <div class="row mt-1">
         <div class="col" style="flex: 0 0 0%;">
-          @if($credito->idevaluacion==1)
-              @if($credito_evaluacion_resumida && $credito->idforma_credito!=1)
-                  @if($users_prestamo->idfuenteingreso == 1)
-                    @php
-                        $res_solvencia_relacion_cuota = $credito_formato_evaluacion ? $credito_formato_evaluacion->resultado_cuota_excedente : 0;
-                    @endphp
-                    @if ($res_solvencia_relacion_cuota > 0 && $res_solvencia_relacion_cuota <= $rango_tope)
-                      <button type="submit" class="btn btn-success">
-                        <i class="fa-solid fa-check"></i> SI, PASAR A PROCESO
-                      </button>
-                    @elseif ($res_solvencia_relacion_cuota > $rango_tope)
-                      @php $validad_eva_resumida = 1; @endphp
-                    @elseif ($res_solvencia_relacion_cuota <= 0)
-                      @php $validad_eva_resumida = 1; @endphp
-                    @else
-                      @php $validad_eva_resumida = 1; @endphp
-                    @endif
-                  @else
-                    @if($credito_evaluacion_resumida->estado_credito_general=='CRÉDITO VIABLE')
-                      <button type="submit" class="btn btn-success">
-                      <i class="fa-solid fa-check"></i> SI, PASAR A PROCESO</button>
-                    @else
-                    <?php
-                    $validad_eva_resumida = 1;
-                    ?>
-                    @endif
-                  @endif
+          @if($credito->idevaluacion==1) {{-- Resumida --}}
+            @if($credito_evaluacion_resumida && $credito->idforma_credito!=1)
+             {{-- Independiente --}}
+              @if($users_prestamo->idfuenteingreso == 1)
+                @php
+                  $res_solvencia_relacion_cuota = $credito_formato_evaluacion ? $credito_formato_evaluacion->resultado_cuota_excedente : 0;
+                @endphp
+                @if ($res_solvencia_relacion_cuota > 0 && $res_solvencia_relacion_cuota <= $rango_tope)
+                  <button type="submit" class="btn btn-success">
+                    <i class="fa-solid fa-check"></i> SI, PASAR A PROCESO
+                  </button>
+                @elseif ($res_solvencia_relacion_cuota > $rango_tope)
+                  @php $validad_eva_resumida = 1; @endphp
+                @elseif ($res_solvencia_relacion_cuota <= 0)
+                  @php $validad_eva_resumida = 1; @endphp
+                @else
+                  @php $validad_eva_resumida = 1; @endphp
+                @endif
               @else
-                  @if($users_prestamo->idfuenteingreso == 2)
-                    @php
-                        $res_solvencia_relacion_cuota = $credito_formato_evaluacion ? $credito_formato_evaluacion->resultado_cuota_excedente : 0;
-                    @endphp
-                    @if ($res_solvencia_relacion_cuota > 0 && $res_solvencia_relacion_cuota <= $rango_tope)
-                      <button type="submit" class="btn btn-success">
-                        <i class="fa-solid fa-check"></i> SI, PASAR A PROCESO
-                      </button>
-                    @elseif ($res_solvencia_relacion_cuota > $rango_tope)
-                      @php $validad_eva_resumida = 1; @endphp
-                    @elseif ($res_solvencia_relacion_cuota <= 0)
-                      @php $validad_eva_resumida = 1; @endphp
-                    @else
-                      @php $validad_eva_resumida = 1; @endphp
-                    @endif
-                  @else
-                    @if($credito->idforma_credito==1 && $validadar_custodia==1)
-                      <button type="submit" class="btn btn-success">
-                      <i class="fa-solid fa-check"></i> SI, PASAR A PROCESO</button>
-                    @endif
-                  @endif
+                @if($credito_evaluacion_resumida->estado_credito_general=='CRÉDITO VIABLE')
+                  <button type="submit" class="btn btn-success">
+                  <i class="fa-solid fa-check"></i> SI, PASAR A PROCESO</button>
+                @else
+                <?php
+                $validad_eva_resumida = 1;
+                ?>
+                @endif
               @endif
-          @else
-          @if($estado_imprimir==0 && $validadar_ampliacion==0)
-          <button type="submit" class="btn btn-success">
-            <i class="fa-solid fa-check"></i> SI, PASAR A PROCESO</button>
-          @endif
+            @else
+              {{-- Dependiente --}}
+              {{-- @if($users_prestamo->idfuenteingreso == 2)
+                @php
+                  $res_solvencia_relacion_cuota = $credito_formato_evaluacion ? $credito_formato_evaluacion->resultado_cuota_excedente : 0;
+                @endphp
+                @if ($res_solvencia_relacion_cuota > 0 && $res_solvencia_relacion_cuota <= $rango_tope)
+                  <button type="submit" class="btn btn-success">
+                    <i class="fa-solid fa-check"></i> SI, PASAR A PROCESO
+                  </button>
+                @elseif ($res_solvencia_relacion_cuota > $rango_tope)
+                  @php $validad_eva_resumida = 1; @endphp
+                @elseif ($res_solvencia_relacion_cuota <= 0)
+                  @php $validad_eva_resumida = 1; @endphp
+                @else
+                  @php $validad_eva_resumida = 1; @endphp
+                @endif
+              @else --}}
+                @if($credito->idforma_credito==1 && $validadar_custodia==1)
+                  <button type="submit" class="btn btn-success">
+                  <i class="fa-solid fa-check"></i> SI, PASAR A PROCESO</button>
+                @endif
+              {{-- @endif --}}
+            @endif
+          @else {{-- 2 Completo, otros --}}
+            @if($estado_imprimir==0 && $validadar_ampliacion==0)
+              <button type="submit" class="btn btn-success">
+                <i class="fa-solid fa-check"></i> SI, PASAR A PROCESO
+              </button>
+            @endif
           @endif
         </div>
         <div class="col" style="flex: 1 0 0%;">

f8987e3dc7b4c7533657be0e95d889a3dbd9c16c
abrahamalanya <alanyaoficial@gmail.com>
2026-04-23 15:16:09 -0500

    propuesta credito

--- resources/views/layouts/backoffice/sistema/credito/propuesta_credito.blade.php
+++ resources/views/layouts/backoffice/sistema/credito/propuesta_credito.blade.php
@@ -1946,7 +1946,7 @@
                     <td class="campo_moneda">{{ $limites_financiamiento_vru }}</td>
                     <td><span style="{{$limites_financiamiento_vru_style}}">{{ $limites_financiamiento_vru_res }}</span></td>
                     <td colspan=2>{{ $credito_propuesta ? $credito_propuesta->limites_financiamiento_vru_res_coment : '' }}</td>
-                    <td></td>
+                    <td>Máximo exigido {{ configuracion($tienda->id,'capital_asignado')['valor'] }} %</td>
                   </tr>
                   @if($credito->idevaluacion == 1)
                     <tr>
@@ -1972,7 +1972,7 @@
                       <td class="doble-subrayado  campo_moneda">{{ $limites_numero_entidades }}</td>
                       <td><span class="doble-subrayado" style="{{$limites_numero_entidades_style}}">{{ $limites_numero_entidades_res }}</span></td>
                       <td colspan=2><input type="text" {{ $view_detalle=='false' ? 'disabled' : '' }} class="form-control color_cajatexto doble-subrayado" value="{{ $credito_propuesta ? $credito_propuesta->limites_numero_entidades_res_coment : '' }}"></td>
-                      <td></td>
+                      <td class="doble-subrayado">Entidades {{ configuracion($tienda->id,'entidades_maxima')['valor'] }}</td>
                     </tr>
                   @endif
                 </tbody>
@@ -2019,7 +2019,7 @@
                   <td class="campo_moneda">{{ $limites_financiamiento_vru }}</td>
                   <td><span style="{{$limites_financiamiento_vru_style}}">{{ $limites_financiamiento_vru_res }}</span></td>
                   <td colspan=2>{{ $credito_propuesta ? $credito_propuesta->limites_financiamiento_vru_res_coment : '' }}</td>
-                  <td>Máximo exigido %</td>
+                  <td>Máximo exigido {{ configuracion($tienda->id,'capital_asignado')['valor'] }} %</td>
                 </tr>
                 <tr>
                   @php
@@ -2048,7 +2048,7 @@
                   <td class="doble-subrayado campo_moneda">{{ $limites_numero_entidades }}</td>
                   <td><span class="doble-subrayado" style="{{$limites_numero_entidades_style}}">{{ $limites_numero_entidades_res }}</span></td>
                   <td class="doble-subrayado"colspan=2>{{ $credito_propuesta ? $credito_propuesta->limites_numero_entidades_res_coment : '' }}</td>
-                  <td class="doble-subrayado">Entidades</td>
+                  <td class="doble-subrayado">Entidades {{ configuracion($tienda->id,'entidades_maxima')['valor'] }}</td>
                 </tr>
               </tbody>
             </table>

733364bd36392519b152416cfe33210e46443639
abrahamalanya <alanyaoficial@gmail.com>
2026-05-11 15:41:13 -0500

    texto en opciones

--- resources/views/layouts/backoffice/sistema/credito/opciones.blade.php
+++ resources/views/layouts/backoffice/sistema/credito/opciones.blade.php
@@ -35,7 +35,7 @@
         onclick="modal({ route:'{{url('backoffice/'.$tienda->id.'/credito/'.$credito->id.'/edit?view=cronograma')}}', size: 'modal-fullscreen' })">
         CRONOGRAMA
       </button>
-      <span style="float: right; color: #e80505; margin-top: 13px;">¡Al modificar datos registrados, volver a guardar en las hojas!</span>
+      <span style="float: right; color: #c40000; margin-top: 13px;">¡Al modificar datos registrados, volver a guardar en las hojas!</span>
     </div>
     <hr style="margin-top: 8px;margin-bottom: 8px;">
     @if($credito->conevaluacion == 'SI')

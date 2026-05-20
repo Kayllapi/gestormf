9adee7e647abb1588bce845836d787ef9bf013d0
abrahamalanya <alanyaoficial@gmail.com>
2026-05-20 16:19:55 -0500

    calculo compuesto

--- app/Http/Controllers/Layouts/Backoffice/Sistema/CalculoCompuestoController.php
+++ app/Http/Controllers/Layouts/Backoffice/Sistema/CalculoCompuestoController.php
@@ -193,7 +193,7 @@ class CalculoCompuestoController extends Controller
                         <td class="text-end">'.$value['amortizacion'].'</td>
                         <td class="text-end">'.$value['interes'].'</td>
                         <td class="text-end">'.$value['cargo'].'</td>
-                        <td class="text-end">'.$value['cuotafinal'].'</td>
+                        <td class="text-end">'.$value['cuota'].'</td>
                         <td class="text-end">'.$value['comision'].'</td>
                         <td class="text-end">'.$value['cuotafinal'].'</td>
                       </tr>';
@@ -205,7 +205,7 @@ class CalculoCompuestoController extends Controller
                         <th class="text-end">'.$cronograma['total_amortizacion'].'</th>
                         <th class="text-end">'.$cronograma['total_interes'].'</th>
                         <th class="text-end">'.$cronograma['total_cargo'].'</th>
-                        <th class="text-end">'.$cronograma['total_cuotafinal'].'</th>
+                        <th class="text-end">'.$cronograma['total_cuota'].'</th>
                         <th class="text-end">'.$cronograma['total_comision'].'</th>
                         <th class="text-end">'.$cronograma['total_cuotafinal'].'</th>
                       </tr>';

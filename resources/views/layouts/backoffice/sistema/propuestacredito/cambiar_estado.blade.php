25d86310408b22f7b1d83569b82e0e52572d8215
abrahamalanya <alanyaoficial@gmail.com>
2026-05-30 10:15:28 -0500

    tabla desembolso

--- resources/views/layouts/backoffice/sistema/propuestacredito/cambiar_estado.blade.php
+++ resources/views/layouts/backoffice/sistema/propuestacredito/cambiar_estado.blade.php
@@ -7,7 +7,7 @@
       }
     </style>
     <div class="modal-header">
-        <h5 class="modal-title">ESTADO DE CRÉDITO</h5>
+        <h5 class="modal-title">APROBACIÓN DE CRÉDITO</h5>
         <button type="button" class="btn-close" id="modal-close-cambiar-estado" data-bs-dismiss="modal" aria-label="Close"></button>
     </div>
     <div class="modal-body" style="height: calc(100vh - 38px);">
